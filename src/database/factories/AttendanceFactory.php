80ad21565075567d1f3a850c8212a9adf7e14811
kazuya0290 <kzytty199120@yahoo.co.jp>
2024-09-16 21:37:15 +0900

    リモートリポジトリの変更

--- src/database/factories/AttendanceFactory.php
+++ src/database/factories/AttendanceFactory.php
@@ -14,17 +14,16 @@ class AttendanceFactory extends Factory
      */
     public function definition()
     {
-        // `start_time` と `end_time` を生成する
+        
         $startTime = $this->faker->dateTimeThisYear();
-        $endTime = $this->faker->dateTimeBetween($startTime, 'now'); // `start_time` より後の時間を生成
-
+        $endTime = $this->faker->dateTimeBetween($startTime, 'now'); 
         return [
             'user_id' => User::factory(),
             'name' => $this->faker->name,
             'start_time' => $startTime,
             'end_time' => $endTime,
-            'rest_time' => $this->faker->numberBetween(0, 3600), // 0秒から1時間（3600秒）までの整数
-            'total' => $this->faker->numberBetween(0, 86400), // 0秒から1日の秒数（86400秒）までの整数
+            'rest_time' => $this->faker->numberBetween(0, 3600), 
+            'total' => $this->faker->numberBetween(0, 86400), 
         ];
     }
 }
