afbb142435691ab00bd235f07319dcae0423fd7f
kazuya0290 <kzytty199120@yahoo.co.jp>
2024-09-23 22:43:22 +0900

    Update .gitignore to exclude Zone.Identifier

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,25 +5,24 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AttendanceController;
 use Illuminate\Support\Facades\Auth;
 
-// 認証関連のルート
+
 Auth::routes();
 
-// ホーム画面のルート
+
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// 勤怠管理のルート定義
-// 認証が必要なルートに 'auth' ミドルウェアを適用
+
 Route::middleware('auth')->group(function () {
     Route::get('/attendance', [AttendanceController::class, 'index']);
 
     Route::get('/attendances', [AttendanceController::class, 'index'])->name('attendances.index');
 
-    // 勤務開始ボタンのルート
+   
     Route::post('/attendance/start', [AttendanceController::class, 'start'])->name('attendance.start');
 
-    // 勤務終了ボタンのルート
+   
     Route::post('/attendance/end', [AttendanceController::class, 'end'])->name('attendance.end');
 
-    // 休憩記録ボタンのルート
+    
     Route::post('/attendance/recordRest', [AttendanceController::class, 'recordRest'])->name('attendance.recordRest');
 });
