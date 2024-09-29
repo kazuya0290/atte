2d690063503d35c2a8df682293df742dedcdc231
kazuya0290 <kzytty199120@yahoo.co.jp>
2024-09-29 17:09:02 +0900

    リモートリポジトリの変更

--- docker/php/init.sh
+++ docker/php/init.sh
@@ -6,10 +6,10 @@ php artisan config:cache || { echo "config:cache failed"; exit 1; }
 php artisan view:cache || { echo "view:cache failed"; exit 1; }
 
 # データベースに接続できるか確認
-if ! mysqladmin ping -h mysql -u laravel_user -p"laravel_pass" --silent; then
-  echo "Database is not reachable, exiting."
-  exit 1
-fi
+until php -r 'new PDO("mysql:host=mysql;dbname=laravel_db", "laravel_user", "laravel_pass");' ; do
+  echo "Waiting for database connection..."
+  sleep 5
+done
 
 # マイグレーションの実行
 php artisan migrate --force || { echo "migrate failed"; exit 1; }
