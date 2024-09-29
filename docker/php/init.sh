8aa29d11afe46f2d817be218301eba5a46944fdb
kazuya0290 <kzytty199120@yahoo.co.jp>
2024-09-29 16:49:05 +0900

    リモートリポジトリの変更

--- docker/php/init.sh
+++ docker/php/init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Laravelのキー生成とキャッシュクリアを実行
+# .envファイルがなくても環境変数が設定されていれば問題ないため、確認を省略
 php artisan key:generate || { echo "key:generate failed"; exit 1; }
 php artisan config:cache || { echo "config:cache failed"; exit 1; }
 php artisan view:cache || { echo "view:cache failed"; exit 1; }
